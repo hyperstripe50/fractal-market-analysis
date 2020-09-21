f75853adeed993b16adc0b885ea303661a616135
dluftspring <daniel.luftspring@gmail.com>
2020-09-20 22:04:42 -0400

    added argparser with 3 cl args for binomial_cascade example

--- examples/binomial_cascade.py
+++ examples/binomial_cascade.py
@@ -1,10 +1,18 @@
+import argparse
 import matplotlib.pyplot as plt; plt.style.use('ggplot')
 from fma.mmar.timeseries import __compute_multiplicative_cascade
 
 if __name__ == '__main__':
-    x, y = __compute_multiplicative_cascade(4, [0.6, 0.4], False)
+
+    parser=argparse.ArgumentParser(description='get options for multifractal measure generator')
+    parser.add_argument('-iters',dest='iters',type=int,help='number of iterations',default=4)
+    parser.add_argument('-hurst',dest='H',type=float,help='Hurst exponent',default=0.6)
+    parser.add_argument('--randomize',dest='shuffle',type=bool,help='Shuffle parameter',default=False)
+    args=parser.parse_args()
+
+    x, y = __compute_multiplicative_cascade(args.iters, [args.H, 1-args.H], args.shuffle)
     plt.step(x, y, where='post')
     plt.ylim(bottom=0)
     plt.xlim(0)
-    plt.xticks(x)
+    # plt.xticks(x)
     plt.show()

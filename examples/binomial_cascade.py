645d9aa627fbe22071c8446279e7912c895d4034
dluftspring <daniel.luftspring@gmail.com>
2020-09-22 09:19:44 -0400

    confirm suggested argparse changes in examples/binomial_cascade.py
    Co-authored-by: hyperstripe50 <jscott28@u.rochester.edu>

--- examples/binomial_cascade.py
+++ examples/binomial_cascade.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt; plt.style.use('ggplot')
 from fma.mmar.timeseries import __compute_multiplicative_cascade
 
 if __name__ == '__main__':
-
     parser=argparse.ArgumentParser(description='get options for multifractal measure generator')
-    parser.add_argument('-iters',dest='iters',type=int,help='number of iterations',default=4)
-    parser.add_argument('-hurst',dest='H',type=float,help='Hurst exponent',default=0.6)
-    parser.add_argument('--randomize',dest='shuffle',type=bool,help='Shuffle parameter',default=False)
+    parser.add_argument('--iters',dest='iters',type=int,help='number of iterations',default=4)
+    parser.add_argument('--alloc',dest='alloc',type=float, nargs='+', help='M-1 allocations of mass',default=[0.6])
+    parser.add_argument('--randomize',dest='randomize',type=bool,nargs='?',help='Randomize order of M',const=True)
     args=parser.parse_args()
 
-    x, y = __compute_multiplicative_cascade(args.iters, [args.H, 1-args.H], args.shuffle)
+    M = np.append(args.alloc, 1 - np.sum(args.alloc))
+    x, y = __compute_multiplicative_cascade(args.iters, M, args.randomize)
     plt.step(x, y, where='post')
     plt.ylim(bottom=0)
     plt.xlim(0)

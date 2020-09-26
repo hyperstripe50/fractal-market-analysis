299def114bc805832317b6a8b0f8c266da94234d
Jona Scott <jonascott@Jessicas-MacBook-Pro.local>
2020-09-25 23:57:12 -0400

    add some color to bmmt

--- examples/bmmt.py
+++ examples/bmmt.py
@@ -6,4 +6,15 @@ if __name__ == '__main__':
     x1, y1 = np.array(__simulate_bmmt(9, M=[.6, .4], randomize=True))
 
     plt.plot(x1,y1, 'b-')
+
+    z1 = np.array(y1)
+    z2 = np.array([0] * len(y1))
+
+    plt.fill_between(x1, y1, 0,
+                 where=(z1 < z2),
+                 alpha=0.30, color='red', interpolate=True)
+    plt.fill_between(x1, y1, 0,
+                 where=(z1 >= z2),
+                 alpha=0.30, color='green', interpolate=True)
+
     plt.show()

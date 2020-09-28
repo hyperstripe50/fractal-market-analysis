7acdaf2e79cfaf584d88de25e31acd901305780b
Jona <jrscott.w@gmail.com>
2020-09-28 11:29:03 -0700

    Fix tests

--- tests/mmar/test_timeseries.py
+++ tests/mmar/test_timeseries.py
@@ -1,7 +1,10 @@
-from fma.mmar.timeseries import __compute_multiplicative_cascade
+from fma.mmar.multiplicative_cascade import MutiplicativeCascade
 
 def test_multiplicative_cascade():
-    x, y = __compute_multiplicative_cascade(1, [0.6, 0.4], False)
+    c = MutiplicativeCascade(1, [0.6, 0.4], False)
+    c.cascade()
+
+    x, y = c.x, c.y
 
     assert all(a == b for a, b in zip(x, [0, 0.5, 1]))
     assert all(a == b for a, b in zip(y, [0, 1.2, 0.8]))

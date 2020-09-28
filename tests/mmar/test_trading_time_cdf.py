10ea0af98820e04041edcd6c51e04af2635529bc
Jona <jrscott.w@gmail.com>
2020-09-28 11:29:03 -0700

    Fix tests

--- tests/mmar/test_trading_time_cdf.py
+++ tests/mmar/test_trading_time_cdf.py
@@ -1,12 +1,7 @@
 from fma.mmar.trading_time_cdf import TradingTimeCDF
 
 def test_diff_at_index_with_pct():
-    cdf = TradingTimeCDF(1, [0.6, 0.4], randomize=False)
-    cdf.compute_cdf()
+    tradingTime = TradingTimeCDF(1, [0.6, 0.4], randomize=False)
+    tradingTime.create_trading_time_cdf()
 
-    # 0th index is from 0 - 0.4
-    # 1st index is from 0.4 - 0.66667
-    # => 0 + (0.4 - 0) * 0.5 = 0.2
-    #    0.4 + (0.66667 - 0.4) * 0.5 = 0.533335
-    # 0.533335 - 0.2 ~= 0.333
-    assert round(cdf.diff_of_two_x(0, .2), 3) == 0.2
+    assert round(tradingTime.cdf(.2) - tradingTime.cdf(0), 3) == 0.2

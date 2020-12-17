9109542563bb6a3bbed51e65359153a6451d9fff
Jonathan Scott <jona@Jonathans-MacBook-Pro.local>
2020-12-16 21:05:08 -0500

    fix examples

---
+++ examples/brownian_motion.py
@@ -0,0 +1,33 @@
+from fma.mmar.brownian_motion import BrownianMotion
+import matplotlib.pyplot as plt; plt.style.use('ggplot')
+from scipy import interpolate
+import numpy as np
+
+bm =  BrownianMotion(9, .457, .603, randomize_segments=True)
+data = bm.simulate() # [ [x, y], ..., [x_n, y_n]]
+
+f = interpolate.interp1d(data[:,0], data[:,1])
+
+y = f(np.arange(0, 1, .001))
+x = np.linspace(0, 1, len(y), endpoint=True)
+
+y_diff = [b - a for a, b in zip(y[:-1], y[1:])]
+
+fig, axs = plt.subplots(2)
+fig.suptitle('MMAR')
+
+axs[0].plot(x, y, 'b-')
+axs[1].plot(x[:-1], y_diff)
+
+z1 = np.array(y)
+z2 = np.array([0] * len(y))
+
+axs[0].fill_between(x, y, 0,
+                where=(z1 >= z2),
+                alpha=0.30, color='green', interpolate=True)
+
+axs[0].fill_between(x, y, 0,
+                where=(z1 < z2),
+                alpha=0.30, color='red', interpolate=True)
+
+plt.show()

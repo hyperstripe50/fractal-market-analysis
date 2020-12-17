b2ffa7783df4a99ca7edd19574624bb7bb721a79
Jonathan Scott <jona@Jonathans-MacBook-Pro.local>
2020-12-16 21:27:52 -0500

    setup tools

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 setup(name="fma",
       version='0.0.1',
       description='Implementation of Benoit Mandelbrot\'s Brownian Motion in Multifractal Time and Edgar E. Peter\'s Fractal Market Analysis.',
-      author='malhar',
+      author='Jona Scott & Daniel Luftspring',
       author_email='jrscott.w@gmail.com',
+      url='https://github.com/hyperstripe50/fractal-market-analysis',
       license='MIT',
       packages=find_packages(),
       install_requires=[

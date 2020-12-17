8b17f030b1e4e1a10f66464c97b3a7764e9f098d
Jonathan Scott <jona@Jonathans-MacBook-Pro.local>
2020-12-16 21:24:18 -0500

    setup tools

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name="fma",
+      version='0.0.1',
+      description='Implementation of Benoit Mandelbrot\'s Brownian Motion in Multifractal Time and Edgar E. Peter\'s Fractal Market Analysis.',
+      author='malhar',
+      author_email='jrscott.w@gmail.com',
+      license='MIT',
       packages=find_packages(),
       install_requires=[
           'numpy',

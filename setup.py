130dae11ec60377692c2b756568a888f3936a03a
Jonathan Scott <jona@Jonathans-MacBook-Pro.local>
2020-12-16 21:53:55 -0500

    1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="fractalmarkets",
-      version='1.0.1-beta',
+      version='1.0.1',
       description='Implementation of Benoit Mandelbrot\'s Brownian Motion in Multifractal Time and Edgar E. Peter\'s Fractal Market Analysis.',
       author='Jona Scott & Daniel Luftspring',
       author_email='jrscott.w@gmail.com',
